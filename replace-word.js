0432bd8746582ae1f4ec359569bd209edbe888cc
Abhishek <abhishek@contact@abhishekprakash.com>
2015-03-04 22:47:00 +0530

    Commiting word-replace.js and readme file - first commit

---
+++ replace-word.js
@@ -0,0 +1,35 @@
+/**
+ * Add a format method to the prototype of String Class
+ * Inorder to achieve python's format method functionality
+ *
+ * Usage - 
+ * var formattedString = "hi $0 $1 $2 $3.".format(["pk", "bk", "ck", "dk"]); //hi pk, bk, ck, dk
+ *
+ * @param {array} [args] [list of strings, that has to be replaced]
+ * @return {string} Returns the string with replaced text.
+ */
+
+String.prototype.format = function(args){
+
+  var str = this.toString(),
+      result = str.match(/\$[0-9]+/gi),
+      mapper = {};
+  
+  
+  if(args.length < result.length)
+    throw new Error("Index out of range.");
+  
+  
+  for(var i = 0; i < result.length; i++){
+    //create a mapper
+    mapper[result[i]] = args[parseInt(result[i].replace("$", ""), 10)]
+  }
+  
+  for(var i = 0; i < result.length; i++){
+    str = str.replace(result[i], mapper[result[i]]);
+  }
+
+  
+  return str; 
+}; 
+
